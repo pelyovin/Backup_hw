986c247dd5391fe3432a224fddcfc4dbe95f2296
Alexander Pelyovin <pelyovin.sanya@yandex.ru>
2024-08-02 22:39:37 +0300

    Added docs and some minor fixes

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from datetime import datetime
 from tqdm import tqdm
 
 
-class Reserv:
+class Backup:
     URL_TO_GET_PHOTOS = ('https://api.vk.com/method/photos.get')
+    access_token_vk = ''
 
-    def __init__(self, access_token, vk_id):
-        self.access_token = access_token
+    def __init__(self, token_ya_disk, vk_id):
+        self.token_ya_disk = token_ya_disk
         self.vk_id = vk_id
 
     def get_photos(self):
+        """Метод делает запрос к апи ВК и возвращает json ответ"""
         params = {
-            'access_token': self.access_token,
+            'access_token': self.access_token_vk,
             'owner_id': self.vk_id,
             'album_id': 'profile',
             'extended': 1,
@@ -24,6 +26,7 @@ class Reserv:
         return response.json()
 
     def file_info(self):
+        """Метод формирует json файл с информацией о фотографиях с указанием названия и типа размера"""
         info = []
         for i in self.get_photos()['response']['items']:
             info.append(
@@ -33,16 +36,18 @@ class Reserv:
             json.dump(info, f)
 
     def to_download(self):
-        photos_dict = {}
+        """Метод формирует словарь с названием фото и ссылок на них для дальнейшего скачивания"""
+        ph_dict = {}
         for i in self.get_photos()['response']['items']:
             for j in i['sizes']:
                 if j['type'] == 'z':
-                    photos_dict[f'{i["likes"]["count"]}_{datetime.fromtimestamp(i["date"]).strftime("%Y-%m-%d")}.png'] = \
-                    j['url']
-        return photos_dict
+                    ph_dict[f'{i["likes"]["count"]}_{datetime.fromtimestamp(i["date"]).strftime("%Y-%m-%d")}.png'] = \
+                        j['url']
+        return ph_dict
 
     def download_photos(self):
-        client = yadisk.Client(token="")
+        """Метод сохраняет фотографии из ВК на Я. диск"""
+        client = yadisk.Client(token=self.token_ya_disk)
         with client:
             if client.exists('photos') and client.is_dir('photos'):
                 pass
@@ -53,9 +58,9 @@ class Reserv:
                     client.upload_url(value, 'photos/' + key)
 
 
-access_token = ''
-vk_id = '155780177'
+token_ya_disk = input('Введите токен с Полигона Яндекс.Диска: ')
+vk_id = input('Введите ваш ВК id: ')
 
-one = Reserv(access_token, vk_id)
+one = Backup(token_ya_disk, vk_id)
 one.file_info()
 one.download_photos()
